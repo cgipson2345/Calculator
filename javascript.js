be542a4181ab5890cd868105f4f20b49d83305be
billblaster <102612368+billblaster@users.noreply.github.com>
2023-11-12 16:28:05 -0800

    added basic math functions

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+const multiply = function (array) {
+    return array.reduce((product, current) => product * current)
+  };
+
+const divide =  function (a, b){  return (a/b); };
